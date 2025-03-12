c45dfc508983db07d9dae8031364f9bdf615d276
bh-premnath-git <Premnath@bighammer.ai>
2025-03-12 08:13:02 +0530

    manual changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  // Load env file based on `mode` in the current directory.
-  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  
   const env = loadEnv(mode, process.cwd(), '')
   
   return {
