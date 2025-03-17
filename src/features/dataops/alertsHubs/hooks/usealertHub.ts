ffe29535d4d535b27633a30d4077033b5f711216
bh-premnath-git <Premnath@bighammer.ai>
2025-03-17 15:19:10 +0530

    added support for both http and https in dev mode

--- src/features/dataops/alertsHubs/hooks/usealertHub.ts
+++ src/features/dataops/alertsHubs/hooks/usealertHub.ts
@@ -1,61 +1,23 @@
 import { useResource } from "@/hooks/api/useResource";
 import { AlertHub } from "@/types/dataops/alertsHub";
-import { toast } from "sonner";
 import { MONITOR_PORT } from "@/config/platformenv";
 
 interface UseAlertHubOptions {
-  shouldFetch?: boolean;
+    shouldFetch?: boolean;
 }
 
 export const useAlertHub = (options: UseAlertHubOptions = { shouldFetch: true }) => {
     const {
         getAll,
-        createMutation,
-        updateMutation,
-        deleteMutation
     } = useResource<AlertHub>('alert', MONITOR_PORT, true);
 
-    const { data: alertHub, isLoading, isFetching, isError } = getAll('/alert/');
-    const updateOne = updateMutation;
-    const deleteOne = deleteMutation;
+    const { data: alertHub, isLoading, isFetching, isError } = getAll({ url: '/alert/' });
 
-    const handleCreateAlertHub = async (data: AlertHub) => {
-        try {
-            await createMutation.mutateAsync(data);
-            toast.success('Data imported successfully');
-        } catch (error) {
-            toast.error('Failed to import the data');
-            throw error;
-        }
-    };
-
-    const handleUpdateAlertHub = async (id: string, data: AlertHub) => {
-        try {
-            await updateOne.mutateAsync({ id, ...data });
-            toast.success('updated successfully');
-        } catch (error) {
-            toast.error('Failed to update');
-            throw error;
-        }
-    };
-
-    const handleDeleteAlertHub = async (id: string) => {
-        try {
-            await deleteOne.mutateAsync({ id });
-            toast.success('deleted successfully');
-        } catch (error) {
-            toast.error('Failed to delete');
-            throw error;
-        }
-    };
 
     return {
         alertHub,
         isLoading,
         isFetching,
         isError,
-        handleCreateAlertHub,
-        handleUpdateAlertHub,
-        handleDeleteAlertHub
     };
 }
